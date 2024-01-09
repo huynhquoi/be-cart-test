ea1af9b0fcbe4a10e1768b14b0c522c0ce9b4f66
VanQuoi <hvquoi.cv@gmail.com>
2024-01-09 09:41:49 +0700

    Update cros

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"start-go/controllers"
 	"start-go/initializers"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +16,19 @@ func init() {
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	router.GET("/shoes", controllers.ShoesIndex)
 	router.GET("/shoes/:id", controllers.ShoesById)
 	router.POST("/shoes", controllers.CreateShoes)
