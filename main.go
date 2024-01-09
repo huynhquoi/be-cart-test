5dceba62d9cd28d9c97e43dc95006fd5856c07d1
VanQuoi <hvquoi.cv@gmail.com>
2024-01-09 10:17:15 +0700

    create cors

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"start-go/controllers"
 	"start-go/initializers"
-	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,17 +15,27 @@ func init() {
 func main() {
 	router := gin.Default()
 
-	router.Use(cors.New(
-		cors.Config{
-			AllowOrigins:     []string{"http://localhost:3000"},
-			AllowMethods:     []string{"GET", "POST"},
-			AllowCredentials: true,
-			MaxAge:           12 * time.Hour,
-		}))
+	router.Use(CORSMiddleware())
 
 	router.GET("/shoes", controllers.ShoesIndex)
 	router.GET("/shoes/:id", controllers.ShoesById)
 	router.POST("/shoes", controllers.CreateShoes)
 
 	router.Run()
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
